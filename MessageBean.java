49a93b3c48ee140f9ccefa756b3fcdf843a0ebcf
daniyal <mohammeddaniyal453@gmail.com>
2025-01-11 19:01:47 +0530

    Added MessageBean.java generated by SetterGetterGenerator

---
+++ MessageBean.java
@@ -0,0 +1,11 @@
+public class MessageBean
+{
+private String message;
+private String heading;
+private boolean generateButtons;
+private boolean generateTwoButtons;
+private String buttonOneText;
+private String buttonTwoText;
+private String buttonOneAction;
+private String buttonTwoAction;
+}
